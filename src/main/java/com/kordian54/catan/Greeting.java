8b7a330f0a91752c78b7d8cd6f93a8420af01865
kordian54 <kordian5446@gmail.com>
2016-12-27 10:40:14 +0100

    Initial project

---
+++ src/main/java/com/kordian54/catan/Greeting.java
@@ -0,0 +1,21 @@
+package com.kordian54.catan;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+	
+}
